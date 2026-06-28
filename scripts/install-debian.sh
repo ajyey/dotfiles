d335d7ace17703688ee5dc66dcc5311d74c14da2
Andrew Leonard <ajyey@andrewleonard.dev>
2026-06-28 02:30:57 -0400

    feat: add build dependencies for Python and native extensions to install script

--- scripts/install-debian.sh
+++ scripts/install-debian.sh
@@ -58,6 +58,18 @@ install_packages() {
     fzf
     fd-find
     wakeonlan
+    build-essential
+    libssl-dev
+    zlib1g-dev
+    libbz2-dev
+    libreadline-dev
+    libsqlite3-dev
+    libncursesw5-dev
+    tk-dev
+    libxml2-dev
+    libxmlsec1-dev
+    libffi-dev
+    liblzma-dev
   )
 
   local available=()
